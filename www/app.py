48aac72aa5464f2287827d1765c514f071794f90
张熇 <hnxyzh917@163.com>
2022-06-17 13:26:00 +0800

    feat: 添加orm

--- www/app.py
+++ www/app.py
@@ -5,6 +5,8 @@ logging.basicConfig(level=logging.INFO)
 import asyncio, os, json, time
 from datetime import datetime
 from aiohttp import web
+import orm
+from models import User, Blog, Comment
 
 
 def index(request):
@@ -25,7 +27,14 @@ async def init(loop):
     # logging.info('server started at http://127.0.0.1:9000...')
     # return srv
 
+
+async def test(loop):
+    await orm.create_pool(loop=loop, user='root', password='123456', db='awesome')
+    u = User(name='Test', email='test@example.com', password='1234567890', image='about:blank')
+    await u.save()
+
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(init(loop))
-    loop.run_forever()
+    # loop.run_until_complete(init(loop))
+    loop.run_until_complete(test(loop))
+    # loop.run_forever()
